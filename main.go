bdf07873c7b79773c00f6f4aa001002b55ed401c
Shivam <shivam.kumar@go-jek.com>
2020-08-07 19:51:53 +0530

    added few more request types

--- main.go
+++ main.go
@@ -13,8 +13,20 @@ var (
 	sugar     = logger.Sugar()
 )
 
-func handleKajabiWebhook(ctx *fasthttp.RequestCtx) {
-	sugar.Infof("received a webhook call!")
+func handleKajabiWebhookPost(ctx *fasthttp.RequestCtx) {
+	sugar.Infof("received a webhook call POST!")
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	sugar.Infof(string(ctx.Request.Body()))
+}
+
+func handleKajabiWebhookGet(ctx *fasthttp.RequestCtx) {
+	sugar.Infof("received a webhook call GET!")
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	sugar.Infof(string(ctx.Request.Body()))
+}
+
+func handleKajabiWebhookPut(ctx *fasthttp.RequestCtx) {
+	sugar.Infof("received a webhook call PUT!")
 	ctx.Response.Header.Set("Content-Type", "application/json")
 	sugar.Infof(string(ctx.Request.Body()))
 }
@@ -24,7 +36,9 @@ func main() {
 	defer logger.Sync() // flushes buffer, if any
 
 	router := fasthttprouter.New()
-	router.POST("/v1/kajabi/send", handleKajabiWebhook)
+	router.POST("/v1/kajabi/send", handleKajabiWebhookPost)
+	router.GET("/v1/kajabi/send", handleKajabiWebhookGet)
+	router.PUT("/v1/kajabi/send", handleKajabiWebhookPut)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
