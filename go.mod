bcaafc3ccf76bdb5a78c40796e9b2a3823dac965
Shivam <shivam.kumar@go-jek.com>
2020-08-09 01:48:59 +0530

    added support for csv to Google Sheet

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/buaazp/fasthttprouter v0.1.1
 	github.com/valyala/fasthttp v1.15.1
 	go.uber.org/zap v1.15.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.30.0
 )
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"log"
+	"encoding/csv"
+	"os"
+	"time"
 
+	"github.com/bhambri94/kajabi-webhook-app/configs"
+	"github.com/bhambri94/kajabi-webhook-app/sheets"
 	"github.com/buaazp/fasthttprouter"
 	"github.com/valyala/fasthttp"
 	"go.uber.org/zap"
@@ -32,7 +36,8 @@ func handleKajabiWebhookPut(ctx *fasthttp.RequestCtx) {
 }
 
 func main() {
-	sugar.Infof("starting kajabi webhook app server...")
+	configs.SetConfig()
+	sugar.Infof("starting kajabi app server...")
 	defer logger.Sync() // flushes buffer, if any
 
 	router := fasthttprouter.New()
@@ -40,5 +45,37 @@ func main() {
 	router.GET("/v1/kajabi/send", handleKajabiWebhookGet)
 	router.PUT("/v1/kajabi/send", handleKajabiWebhookPut)
 
-	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+	// log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+
+	values := ReadCSV(configs.Configurations.CSVName)
+	SheetName := GetSheetName()
+	sheets.ClearSheet(SheetName)
+	sheets.BatchWrite(SheetName, values)
+}
+
+func ReadCSV(file string) [][]interface{} {
+	f, err := os.Open(file)
+	if err != nil {
+
+	}
+	defer f.Close()
+
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+
+	}
+	var finalValues [][]interface{}
+	for _, line := range lines {
+		row := make([]interface{}, len(line))
+		for i, v := range line {
+			row[i] = v
+		}
+		finalValues = append(finalValues, row)
+	}
+	return finalValues
+}
+
+func GetSheetName() string {
+	currentTime := time.Now()
+	return currentTime.Format("2006-01-02T01")
 }
